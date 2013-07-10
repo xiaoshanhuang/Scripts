c6c4a266cf14aa8ece36bf0a02f9b6cfab70f655
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-07-10 15:18:24 -0700

    Disable Mission Control Animation

--- usfcmd.sh
+++ usfcmd.sh
@@ -5,6 +5,8 @@
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 # Disable Rubber Band Effect
 defaults write -g NSScrollViewRubberbanding -int 0
+# Disable Mission Control Animation
+defaults write com.apple.dock expose-animation-duration -int 0; killall Dock
 
 # Resize image
 sips -Z 640 *.jpg
