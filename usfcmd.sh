087252223d3c11b06a707f0968d261e22de268d3
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-07-23 15:16:45 -0700

    subl

--- usfcmd.sh
+++ usfcmd.sh
@@ -10,5 +10,8 @@ defaults write com.apple.dock expose-animation-duration -int 0; killall Dock
 # Speed Up a Slow Terminal by Clearing Log Files
 sudo rm -rf /private/var/log/asl/*.asl
 
+# subl start sublime text in terminal
+ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+
 # Resize image
 sips -Z 640 *.jpg
