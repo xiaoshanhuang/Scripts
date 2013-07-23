d5866d14066a691c9330fff5ccd6aa753a3f0d93
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-07-23 15:12:17 -0700

    speed up terminal

--- usfcmd.sh
+++ usfcmd.sh
@@ -7,6 +7,8 @@ defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 defaults write -g NSScrollViewRubberbanding -int 0
 # Disable Mission Control Animation
 defaults write com.apple.dock expose-animation-duration -int 0; killall Dock
+# Speed Up a Slow Terminal by Clearing Log Files
+sudo rm -rf /private/var/log/asl/*.asl
 
 # Resize image
 sips -Z 640 *.jpg
