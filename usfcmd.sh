846eff3f31403459cdae483cc8404fb0868b5d69
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-07-26 12:23:26 -0700

    add spacer in dock

--- usfcmd.sh
+++ usfcmd.sh
@@ -13,5 +13,10 @@ sudo rm -rf /private/var/log/asl/*.asl
 # subl start sublime text in terminal
 ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
+# add spacer in dock apps
+defaults write com.apple.dock persistent-apps -array-add ‘{tile-data={}; tile-type=“spacer-tile”;}’; killall dock;
+# add spacer in dock others
+defaults write com.apple.dock persistent-others -array-add ‘{tile-data={}; tile-type=“spacer-tile”;}’; killall dock;
+
 # Resize image
 sips -Z 640 *.jpg
