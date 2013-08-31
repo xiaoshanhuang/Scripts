4d61f5bf2e7d396880e5ac9e8ffde4ed3580fb55
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-08-31 15:54:47 +0800

    add new address

--- isatap.sh
+++ isatap.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#清除IPV6路由表
-sudo route delete -inet6 default
  
 EN0_IP=`/sbin/ifconfig en0 | grep inet | grep -v inet6 | awk '{print $2}'` 
 EN1_IP=`/sbin/ifconfig en1 | grep inet | grep -v inet6 | awk '{print $2}'` 
@@ -15,5 +13,9 @@ echo $LOCAL_IP
 if [ -n "$LOCAL_IP" ]; then 
 /sbin/ifconfig gif0 tunnel $LOCAL_IP isatap.tsinghua.edu.cn
 /sbin/ifconfig gif0 inet6 2402:f000:1:1501:200:5efe:$LOCAL_IP prefixlen 64
+/sbin/route delete -inet6 default
 /sbin/route add -inet6 default 2402:f000:1:1501::1
+# /sbin/ifconfig gif0 inet6 2001:da8:200:900e:0:5efe:$LOCAL_IP prefixlen 64
+# /sbin/route delete -inet6 default
+# /sbin/route add -inet6 default 2001:da8:200:900e::1
 fi
