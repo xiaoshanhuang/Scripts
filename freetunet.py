7d576e92ad0fbf9c29b60b713939ee992784372c
Xiaoshan Huang <xiaoshanhuang@gmail.com>
2013-09-13 23:12:18 +0800

    Windows works!!!

--- freetunet.py
+++ freetunet.py
@@ -1,10 +1,10 @@
 #!/bin/usr/env python
 #-*- coding: utf-8 -*-
 #title           : freetunet.py
-#description     :￼config ip for free tunet access
+#description     : config ip for free tunet access
 #author          : Xiaoshan Huang, xiaoshanhuang@gmail.com
-#date            : 2013-09-12 19:34
-#version         : 0.1
+#date            : 2013-09-13 23:11
+#version         : 0.2
 #usage           : python freetunet.py
 #notes           : 
 #python_version  : 2.7
@@ -19,27 +19,76 @@
 # 	'Linux': {'ipv4': "/sbin/ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'", 'ipv6': "/sbin/ifconfig  | grep 'inet6 addr:'| grep 'Global' | grep -v 'fe80' | awk '{print $3}'"}
 # }
 
-# url = "http://net.tsinghua.edu.cn/"
-# baidu = "http://www.baidu.com/"
-
 # def ip_addresses(version):
 # 	proc = subprocess.Popen(commands[platform.system()][version], shell=True,stdout=subprocess.PIPE)
 # 	return proc.communicate()[0].replace('\n', '')
 
 # print ip_addresses('ipv4')
 
+import os
 import urllib2, httplib
-httplib.HTTPConnection.debuglevel = 1
-request = urllib2.Request(baidu)
-request.add_header("Accept", "text/html,*/*")
-request.add_header("Connection", "Keep-Alive")
-opener = urllib2.build_opener()
-f = opener.open(request)
-if f.url == "http://www.baidu.com/":
-	print "WOW"
-else:
-	print "..."
+import time
+url = "http://net.tsinghua.edu.cn/"
+baidu = "http://www.baidu.com/"
 
+def openConnection(request, opener):
+	try:
+		f = opener.open(request)
+	except urllib2.URLError, e:
+		if e.reason[0] == 11001:
+			return 1
+		else:
+			return 0
+
+def checkBaidu():
+	httplib.HTTPConnection.debuglevel = 1
+	request = urllib2.Request(baidu)
+	request.add_header("Accept", "text/html,*/*")
+	request.add_header("Connection", "Keep-Alive")
+	opener = urllib2.build_opener()
+	timeOut = 5
+	count = 0
+	while openConnection(request, opener) and (count<timeOut):
+		count += 1
+		time.sleep(2)
+	if count < timeOut:
+		f = opener.open(request)
+		if f.url == baidu:
+			print 'Connected'
+			return True
+		else:
+			return False
+	else:
+		return False
+
+
+import wmi
+
+wmiService = wmi.WMI()
+colNicConfigs = wmiService.Win32_NetworkAdapterConfiguration(IPEnabled = True)
+if len(colNicConfigs) < 1:
+    exit()
+objNicConfig = colNicConfigs[0]
+
+ipFix = '166.111.153.'
+arrSubnetMasks = ['255.255.254.0']
+for ipSweep in range(1, 232):
+	arrIPAddresses = [ipFix + str(ipSweep)]
+	intReboot = 0
+	returnValue = objNicConfig.EnableStatic(IPAddress = arrIPAddresses, SubnetMask = arrSubnetMasks)
+	if returnValue[0] == 0:
+		print "Current IP:"
+		print arrIPAddresses
+		# os.system('pause')
+		if checkBaidu():
+			print 'WOW'
+			# os.system('pause')
+			exit()
+	elif returnValue[0] == 1:
+	    print "Reboot"
+	    intReboot += 1
+	else:
+	    print "Error"
 
 # from selenium import webdriver
 # import time
